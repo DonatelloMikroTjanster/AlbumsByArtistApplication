94c2254d06db27744fbac2664c3798afe0f50fe7
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-09-29 13:15:15 +0200

    changed field size, data type

--- src/main/java/org/example/edufyalbumforartist/entities/Album.java
+++ src/main/java/org/example/edufyalbumforartist/entities/Album.java
@@ -17,7 +17,7 @@ public class Album {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "name", length = 100, nullable = false)
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
 
 
