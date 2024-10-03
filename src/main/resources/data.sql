e94efab061c9b983e03193868a4a42983c4611a7
AbaSheger <111607144+AbaSheger@users.noreply.github.com>
2024-10-03 17:44:56 +0200

    Lots of changes

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,21 @@
+-- Insert Genres
+INSERT IGNORE INTO genre (id, name)
+VALUES
+    (1, 'Rock'),
+    (2, 'Pop'),
+    (3, 'Technology'),
+    (4, 'Nature');
 
 -- Insert Artists
-INSERT INTO artist (id, name, genre)
+INSERT IGNORE INTO artist (id, name, genre_id)
 VALUES
-    (1, 'The Beatles', 'Rock'),
-    (2, 'Michael Jackson', 'Pop'),
-    (3, 'Elon Musk', 'Technology'),
-    (4, 'David Attenborough', 'Nature');
+    (1, 'The Beatles', 1),
+    (2, 'Michael Jackson', 2),
+    (3, 'Elon Musk', 3),
+    (4, 'David Attenborough', 4);
 
 -- Insert Albums
-INSERT INTO album (id, name, release_date)
+INSERT IGNORE  INTO album (id, name, release_date)
 VALUES
     (1, 'Abbey Road', '1969-09-26'),
     (2, 'Thriller', '1982-11-30'),
@@ -16,7 +23,7 @@ VALUES
     (4, 'Planet Earth II Series', '2016-11-06');
 
 -- Insert Album-Artist relationships
-INSERT INTO album_artist (album_id, artist_id)
+INSERT IGNORE INTO album_artist (album_id, artist_id)
 VALUES
     (1, 1),  -- Abbey Road - The Beatles
     (2, 2),  -- Thriller - Michael Jackson
